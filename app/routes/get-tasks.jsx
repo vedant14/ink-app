0f717ed05bee45a299ba05da4bdff31014eeeb56
Vedant Lohbare <vedantlohbare@Vedants-MacBook-Air.local>
2025-06-30 10:47:34 +0530

    HTML output

--- app/routes/get-tasks.jsx
+++ app/routes/get-tasks.jsx
@@ -33,23 +33,23 @@ export async function loader({ params }) {
 export function HydrateFallback() {
   return <div>Loading...</div>;
 }
-export default function Tasks({ loaderData }) {
-  if (loaderData.error) {
-    return <div>Error: {loaderData.error}</div>;
-  }
-  const { imageData, nextRefreshAfter } = loaderData;
-  const imageSrc = `data:image/bmp;base64,${imageData}`;
+// export default function Tasks({ loaderData }) {
+//   if (loaderData.error) {
+//     return <div>Error: {loaderData.error}</div>;
+//   }
+//   const { imageData, nextRefreshAfter } = loaderData;
+//   const imageSrc = `data:image/bmp;base64,${imageData}`;
 
-  return (
-    <div>
-      <img
-        src={imageSrc}
-        alt="A BMP image showing a list of tasks"
-        style={{ border: "1px solid black", maxWidth: "100%" }}
-      />
-      <p style={{ fontFamily: "monospace", marginTop: "10px" }}>
-        Next refresh after: {nextRefreshAfter}
-      </p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <img
+//         src={imageSrc}
+//         alt="A BMP image showing a list of tasks"
+//         style={{ border: "1px solid black", maxWidth: "100%" }}
+//       />
+//       <p style={{ fontFamily: "monospace", marginTop: "10px" }}>
+//         Next refresh after: {nextRefreshAfter}
+//       </p>
+//     </div>
+//   );
+// }
